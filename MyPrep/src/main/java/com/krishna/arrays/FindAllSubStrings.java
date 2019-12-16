8dd17a7b9b1ed677c4edb0e914d72dca97b63d11
kabothu <kabothu@gmail.com>
2019-12-16 01:33:47 -0600

    Added few more programs

--- MyPrep/src/main/java/com/krishna/arrays/FindAllSubStrings.java
+++ MyPrep/src/main/java/com/krishna/arrays/FindAllSubStrings.java
@@ -1,17 +1,47 @@
 package com.krishna.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//https://www.youtube.com/watch?v=VdnvmfzA1pw&t=801s
+
+class SolutionTest {
+
+	public static List<List<Integer>> subsets(int[] num) {
+
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+
+		if (num == null || num.length == 0) {
+			return results;
+		}
+
+		Arrays.sort(num);
+		List<Integer> subset = new ArrayList<Integer>();
+		toFindAllSubSets(num, results, subset, 0);
+		return results;
+
+	}
+
+	public static void toFindAllSubSets(int[] num, List<List<Integer>> results, List<Integer> subset, int startIndex) {
+
+		results.add(new ArrayList<Integer>(subset));
+
+		for (int i = startIndex; i < num.length; i++) {
+			subset.add(num[i]);
+			toFindAllSubSets(num, results, subset, i + 1);
+			subset.remove(subset.size() - 1);
+		}
+
+	}
+}
+
 public class FindAllSubStrings {
 
 	public static void main(String[] args) {
-		String s1 = "ABC";
-		char[] c1 = s1.toCharArray();
-		for(int i=0; i<c1.length;i++) {
-			for(int j=i; j<c1.length;j++) {
-				for(int k=i;k<=j;k++) {
-					System.out.println(c1[k]+" ");
-				}
-			}
-		}
+
+		int[] nums = { 3, 2, 1 };
+		System.out.println(SolutionTest.subsets(nums));
 
 	}
 
